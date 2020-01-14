83debecaa8637b75817c2a326f0518c7cc861036
Sam Cooper <samcooperpdx@gmail.com>
2020-01-13 21:22:26 -0800

    password security input

--- app.py
+++ app.py
@@ -8,11 +8,14 @@ import requests
 import psycopg2
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
+from getpass import getpass
 
 
 app = Flask(__name__)
 pguser = input('what is your Postgres username?')
-pw = input('what is your postgres password?')
+
+pw = getpass()
+# pw = input('what is your postgres password?')
 engine = create_engine(f'postgresql://{pguser}:{pw}@localhost:52866/USDA_Foods')
 # engine = create_engine(f'postgresql://postgres:Bl@st0ise18@localhost:52866/USDA_Foods')
 print('line 34')
