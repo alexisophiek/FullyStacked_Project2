3c596eb7ce75c8584c987f408b481cb720d0a399
Sam Cooper <samcooperpdx@gmail.com>
2020-01-13 20:52:55 -0800

    fixed the app.py file with user input prompts for pg user and pg password

--- app.py
+++ app.py
@@ -1,34 +1,32 @@
 import pandas as pd
 import json
 from flask_sqlalchemy import SQLAlchemy
-
-# from flask_sqlalchemy import SQLAlchemy
-
+import sqlalchemy
+from sqlalchemy import create_engine, func
 from flask import Flask, render_template, redirect
 import requests
 import psycopg2
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
 
-# engine = create_engine("postgresql+pg8000://dbuser:kx%25jj5%2Fg@pghost10/appdb")
-engine = create_engine("postgres://postgres:Bl@st0ise18@localhost:53703/USDA_Foods")
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
+app = Flask(__name__)
+pguser = input('what is your Postgres username?')
+pw = input('what is your postgres password?')
+engine = create_engine(f'postgresql://{pguser}:{pw}@localhost:52866/USDA_Foods')
+# engine = create_engine(f'postgresql://postgres:Bl@st0ise18@localhost:52866/USDA_Foods')
 
-# Save reference to the table
-Passenger = Base.classes.passenger
+
+print('line 34')
 
 #################################################
 # Flask Setup
 #################################################
-app = Flask(__name__)
 
 @app.route("/")
 def welcome():
     """List all available api routes."""
-    return (
-        f"<h1> Welcome to Foods</h1>"
-    )
+    return (f"<h1> Welcome to Foods</h1>")
 
-
+if __name__ == '__main__':
+	app.run(debug=True)
