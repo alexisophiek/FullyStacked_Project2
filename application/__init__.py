64829cbddafdde4e86fae8b9dcba702d3f6a6826
Alexis <alexisophie.k@gmail.com>
2020-01-20 22:26:03 -0800

    serving correctly

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 
 def create_app():
     """Construct the core application."""
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=False)
     # db.init_app(app)
     # app.config.from_object('config.Config')
 
--- application/routes.py
+++ application/routes.py
@@ -2,18 +2,7 @@ from flask import Flask, render_template, redirect, make_response
 from flask import request, render_template, make_response
 from datetime import datetime as dt
 from flask import current_app as app
-import os
-import json
-
-################################
-# Load in the JSON File
-file = "static/data/counties.json"
-def county(file):
-    with open(file, 'r') as f:
-        data = json.load(f)
-        return data
-
-data = county(file)
+from compiling import county
 
 
 @app.route("/")
@@ -32,6 +21,6 @@ def presentation():
 @app.route("/counties_data")
 def counties_data():
 	print('endpoint HIT~~~~~~~')
-	county(file)
+	county()
 	return jsonify(data)
 
