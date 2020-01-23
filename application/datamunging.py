310cba6f77c1562cf92f6915c9886457af0e5bf7
Sam Cooper <samcooperpdx@gmail.com>
2020-01-22 21:37:30 -0800

    heroku dest for pdb

--- application/datamunging.py
+++ application/datamunging.py
@@ -13,7 +13,8 @@ import sqlalchemy
 # Open a Query Session
 def pgcall():
     # engine = create_engine(f'postgresql://postgres:postgres@localhost:5432/USDA_Foods')
-    engine = create_engine(f'postgresql://postgres:Bl@st0ise18@localhost:5432/USDA_Foods')
+    engine = create_engine(f'postgres://wetdrraelvhobs:b44742c8826d30c52c589b8c5f3ce36a002a69481636a46827c6dfe39efd568f@ec2-3-224-165-85.compute-1.amazonaws.com:5432/db8viu4uv11kb6')
+    # engine = create_engine(f'postgresql://postgres:Bl@st0ise18@localhost:5432/USDA_Foods')
     # From PostgresSQL
     result_set = engine.execute("SELECT * FROM main")  
     # for r in result_set: 
