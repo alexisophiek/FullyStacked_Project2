780f87dac9740744bdf6a568398af2ff5a021607
Sam Cooper <samcooperpdx@gmail.com>
2020-01-23 18:24:00 -0800

    oneachfeature and bindPopup

--- application/static/js/map.js
+++ application/static/js/map.js
@@ -23,19 +23,59 @@ function styles(feature) {
 };
 function counties_endpoint(){
   var endpoint = "/counties_data"
-
-
 // START D3.JSON
       d3.json(endpoint, function(data) {
 
-        var counties = data;
+    var counties = data      
 
         L.geoJson(counties, {
-            style: styles
+            style: styles,
+            onEachFeature: onEachFeature
           })
         .addTo(myMap) 
-       })
 
+       })
+    }
+
+
+function onEachFeature(feature, layer) {
+            var popupContent = "<div>County: " + feature.properties.NAME+
+            "</br>Obesity Rate: " + feature.properties.PCT_OBESE_ADULTS13+
+            "National Avg Obesity Rate: " + feature.properties.natavgobese +
+            "</br>Diabetes Rate: "+feature.properties.PCT_DIABETES_ADULTS13+
+            "National Avg Diabetes Rate: " + feature.properties.natavgdiab +
+            "</br>Overall Low Access to Store: " +feature.properties.PCT_LACCESS_POP15+
+            "</br>SNAP Participants with Low Access to Store: " +feature.properties.PCT_LACCESS_SNAP15+
+            "</br>Median Household Income: " +feature.properties.MEDHHINC15+
+            "National Avg Household Income " + feature.properties.natavgincome 
+            "</div";
+            layer.bindPopup(popupContent)
 }
+
+// function onEachFeature(feature, layer) {
+//             var popupContent = "<div>County: " + feature.properties.NAME+
+//             "</br>Obesity Rate: " + feature.properties.PCT_OBESE_ADULTS13+
+//             "National Avg Obesity Rate: " + natavgobese +
+//             "</br>Diabetes Rate: "+feature.properties.PCT_DIABETES_ADULTS13+
+//             "National Avg Diabetes Rate: " + natavgdiab +
+//             "</br>Overall Low Access to Store: " +feature.properties.PCT_LACCESS_POP15+
+//             "</br>SNAP Participants with Low Access to Store: " +feature.properties.PCT_LACCESS_SNAP15+
+//             "</br>Median Household Income: " +feature.properties.MEDHHINC15+
+//             "National Avg Household Income " + natavgincome 
+//             "</div";
+// }
 counties_endpoint()
 console.log('end')
+
+
+// function onEachFeature(feature, layer) {
+//             var popupContent = "<div>County: " + feature.properties.NAME+
+//             "</br>Obesity Rate: " + feature.properties.PCT_OBESE_ADULTS13+
+//             "National Avg Obesity Rate: " + natavgobese +
+//             "</br>Diabetes Rate: "+feature.properties.PCT_DIABETES_ADULTS13+
+//             "National Avg Diabetes Rate: " + natavgdiab +
+//             "</br>Overall Low Access to Store: " +feature.properties.PCT_LACCESS_POP15+
+//             "</br>SNAP Participants with Low Access to Store: " +feature.properties.PCT_LACCESS_SNAP15+
+//             "</br>Median Household Income: " +feature.properties.MEDHHINC15+
+//             "National Avg Household Income " + natavgincome 
+//             "</div";
