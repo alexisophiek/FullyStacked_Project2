e05399fa0fecb1255e132981905d7660c322ea1d
Alexis <alexisophie.k@gmail.com>
2020-01-23 22:32:39 -0800

    before running heroku again

--- application/static/js/logic.js
+++ application/static/js/logic.js
@@ -1,10 +1,7 @@
+// JS for content editable trick from Chris Coyier
 
-  // Connect to DB and make sure we can query
+var h1 = document.querySelector("h1");
 
-//   var pg = require(‘pg’);
-//   var connectionString ='postgresql://postgres:postgres@localhost:52866/USDA_Foods';
-//   var pgClient = new pg.Client(connectionString);
-//   var query = pgClient.query("QUERY");
-
-
-
+h1.addEventListener("input", function() {
+  this.setAttribute("data-heading", this.innerText);
+});
