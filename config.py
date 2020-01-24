8a09edd518ac03fd383240ed885704397a0871d2
Alexis <alexisophie.k@gmail.com>
2020-01-23 17:45:40 -0800

    fixed pgadmin4

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # from os import environ
 
 
-# class Config:
+class Config:
 #     """Set Flask configuration vars from .env file."""
 
 #     # General Config
@@ -10,5 +10,4 @@
 #     FLASK_ENV = environ.get('FLASK_ENV')
 
 #     # Database
-#     SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
-#     SQLALCHEMY_TRACK_MODIFICATIONS = environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
+    PG_DATABASE_URL = "postgres://sgbwpichcpvxtd:bf4d337f0f30d387d741df43d5e2a6d198fd60fa7abbba613abd6687a3066c33@ec2-3-224-165-85.compute-1.amazonaws.com:5432/deq8hmr80e27ob"
