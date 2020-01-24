c287f76fda008b24bcf0ab4fe3a41e2322a62fa3
Sam Cooper <samcooperpdx@gmail.com>
2020-01-24 09:38:11 -0800

    revert encoding

--- application/datamunging.py
+++ application/datamunging.py
@@ -6,43 +6,42 @@ from sqlalchemy import create_engine
 from sqlalchemy import func
 from sqlalchemy import *
 import sqlalchemy
+import unicodedata
+from bs4 import UnicodeDammit
 
 
 
 ################################
 # Open a Query Session
 def pgcall():
-    # engine = create_engine(f'postgresql://postgres:postgres@localhost:5432/USDA_Foods')
-    engine = create_engine(f'postgres://wetdrraelvhobs:b44742c8826d30c52c589b8c5f3ce36a002a69481636a46827c6dfe39efd568f@ec2-3-224-165-85.compute-1.amazonaws.com:5432/db8viu4uv11kb6')
-    # engine = create_engine(f'postgresql://postgres:Bl@st0ise18@localhost:5432/USDA_Foods')
-    # From PostgresSQL
-    result_set = engine.execute("SELECT * FROM main")
-    # for r in result_set: 
-    result = {}
-    go = [dict(r) for r in result_set]
-    for i in go:
-        result[i['NAME']] = i
-        # print(i)
-    return result
+	# engine = create_engine(f'postgresql://postgres:postgres@localhost:5432/USDA_Foods')
+	engine = create_engine(f'postgres://wetdrraelvhobs:b44742c8826d30c52c589b8c5f3ce36a002a69481636a46827c6dfe39efd568f@ec2-3-224-165-85.compute-1.amazonaws.com:5432/db8viu4uv11kb6')
+	# engine = create_engine(f'postgresql://postgres:Bl@st0ise18@localhost:5432/USDA_Foods')
+	# From PostgresSQL
+	result_set = engine.execute("SELECT * FROM main")
+	# for r in result_set: 
+	result = {}
+	go = [dict(r) for r in result_set]
+	for i in go:
+		result[i['NAME']] = i
+		# print(i)
+	return result
 pgcall()
 
 ################################
 # Load in the JSON File
 file = os.path.join('application','static','data','counties.json')
-
 def county(file, pgjson):
-    with open(file, 'r', encoding = "ISO-8859-1") as f:
-        data = json.load(f)
-    for each in data["features"]:
-        # print(each)
-        county = each['properties']['NAME']
-        new_attr = pgjson.get(county)
-        if new_attr:
-            each['properties'].update(new_attr)
-        else:
-            print(county)
-    # print(data)    
-    return data
+	with open(file, 'r') as f:
+		data = json.load(f)
+	for each in data["features"]:
+		county = each['properties']['NAME']
+		new_attr = pgjson.get(county)
+		if new_attr:
+			each['properties'].update(new_attr)
+		else:
+			print(county)
+	return data
 
 ## Name pgcall and Call county funct
 pgjson = pgcall()
--- application/routes.py
+++ application/routes.py
@@ -2,26 +2,46 @@ from flask import Flask, render_template, redirect, make_response
 from flask import request, render_template, make_response
 from flask import current_app as app
 from flask import jsonify
+import flask
+flask.__version__
 import os
 from .datamunging import pgcall, county
+import unicodedata
+
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('index.html', title='Home')
+	return render_template('index.html', title='Home')
 
 @app.route("/map")
 def map():
-    return render_template('map.html', title="Map")
+	return render_template('map.html', title="Map")
 
 @app.route("/presentation")
 def presentation():
-    return render_template('presentation.html', title='Presentation')
+	return render_template('presentation.html', title='Presentation')
 
 @app.route("/counties_data")
 def counties_data():
-    file = os.path.join('application','static','data','counties.json')
-    pgjson = pgcall()
-    data = county(file, pgjson)
-    return jsonify(data)
+	file = os.path.join('application','static','data','counties.json')
+	pgjson = pgcall()
+	# data = county(file, pgjson)
+	data = county(file, pgjson)
+	print(data)
+	return jsonify(data)
+
+	# def read_file(filename, charset='utf-8'):
+	# with open(filename, 'r') as f:return f.read().decode(charset)
+
+
+
+	# return jsonify(data)
+
+
+
+
+@app.route("/pgcall")
+def pg():
+	print(pgcall())
